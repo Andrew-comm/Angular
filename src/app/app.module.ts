b5f9bf3104534ff678ed4632712086e62ed63c63
Andrew Rono <ndrwrono2001@gmail>
2022-11-05 13:14:34 +0300

    configuring angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
